59d845381308a448a89ad1b632f3560b05a9623b
Chris Calderon <chris-da-dev@augur.net>
2015-04-23 17:21:41 -0700

    changing ss.py

--- ss.py
+++ ss.py
@@ -1,30 +1,55 @@
 #!/usr/bin/python
+import warnings; warnings.simplefilter('ignore')
 from colorama import Style, Fore, init
 from collections import defaultdict
 from gethrpc import GethRPC
 import leveldb
 import serpent
+import json
 import sys
 import os
 import re
 
-def 
-
-IMPORTP = re.compile(r'^import (?P<path>.*) as (?P<macro>)$', re.M)
+DB = leveldb.LevelDB('/.build')
+IMPORTP = re.compile(r'^import (?P<path>.*) as (?P<macro>.*)$', re.M)
 ERROR = Style.BRIGHT + Fore.RED + 'ERROR!'
-filedata = {}
-dependencies = defaultdict(list)
-paths = sys.argv[1:]
-for path in paths:
-    assert os.path.exists(path), ERROR + ' Path does not exist: %s' % path
-    for top_dir, _, filenames in os.walk(path):
-        for filename in filenames:
-            filename = os.path.join(top_dir, filename)
-            if filename.endswith('.se'):
-                filedata[filename] = open(filename).read()
-                for match in IMPORTP.finditer(filedata[filename]):
-                    d = match.groupdict()
-                    d['path'] = os.path.abspath(d['path'])
-                    dependencies[d['path']].append((filename, d['macro']))
 
+def depth(filename, count=0):
+    return max((count if filename not in dependencies[f] else depth(f, count+1)) for f in dependencies)
 
+def default():
+    return {'date':0.0, 'sig':'', 'fullsig':'', 'address':''}
+
+def load(filename):
+    try:
+        return json.loads(db.Get(filename))
+    except:
+        return default()
+
+def macro(address, name):
+    return 'macro %(name)s:\n    %(address)s\n'%locals()
+
+def main():
+    filedata = {}
+    dependencies = defaultdict(list)
+    imports = defaultdict(list)
+    paths = sys.argv[1:]
+    for path in paths:
+        assert os.path.exists(path), ERROR + ' Path does not exist: %s' % path
+        os.chdir(path)
+        
+
+    fileinfo = {}
+    for d, filename in sorted(zip(map(depth, dependencies), dependencies)):
+        info = load(filename)
+        date = os.path.getmtime(filename)
+        if info['date'] < date:
+            info['date'] = date
+            info['sig'] = serpent.mk_signature(filename)
+            info['fullsig'] = serpent.mk_full_signature(filename)
+            for m, d in imports[filename]:
+                a = fileinfo[d['path']]['address']
+                s = fileinfo[d['path']]['sig']
+                import_line = m.string[m.start():m.end()]
+                replacement = s + '\n' + macro(a, d['macro'])
+                filedata[filename].replace(
